10123702a45ec76709352d3ed6c4cd857a11f4eb
PurwantoGz <purwanto@outlook.com>
2016-06-15 13:29:01 +0700

    Thread using Lambda Expressions

--- MultiThread/MultiThread/Program.cs
+++ MultiThread/MultiThread/Program.cs
@@ -2,73 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThread
 {
     #region Class Program
     class Program
     {
-        static bool done;
-        static readonly object locker = new object();
-        static void Main(string[] args)
-        {
-            /* 1. Make Thread
-            Thread t = new Thread(writeY);
+        /*PASSING DATA to Thread*/
+        static void Main(string[] args) {
+            //Thread t = new Thread(()=>Print("Hello, Dunia"));
+            Thread t = new Thread(()=> 
+            {
+                Console.WriteLine("I`m running on another thread");
+                Console.WriteLine("This is so easy");
+            });
             t.Start();
-
-            for (int i = 0; i < 1000; i++)
-            {
-                Console.Write("X");
-            }
-            */
-
-            /*2. 
-            new Thread(Go).Start(); // Call Go() on a new Thread
-            Go(); // Call Go() as a function main.
-            */
-
-            /*3. Thread a same instance
-            Program tt = new Program();
-            new Thread(tt._Go).Start();
-            tt._Go();
-            */
-            new Thread(Go1).Start();
-            Go1();
-            Console.ReadLine();           
+            Console.ReadLine();
         }
+        static void Print(string message) { Console.WriteLine(message); }
+        
 
-        #region Function
-        static void Go1()
-        {
-            lock (locker)
-            {
-                if (!done) { Console.WriteLine("Done");done = true; }
-            }
-        }
-
-        void _Go()
-        {
-            if (!done)
-            {
-                Console.WriteLine("Done"); done = true;
-            }
-        }
-        static void Go()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write("?");
-            }
-        }
-
-        static void writeY()
-        {
-            for (int i = 0; i < 1000; i++) Console.Write("y");
-            Console.WriteLine();
-        }
-        #endregion
+        
     }
     #endregion
 
