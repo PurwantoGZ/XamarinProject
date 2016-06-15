35df11ff5695a28f0ca612b5da161b57ced6c6cd
PurwantoGz <purwanto@outlook.com>
2016-06-15 12:08:40 +0700

    Learn Multi Threading in C#

---
+++ MultiThread/MultiThread/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultiThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1. Make Thread
+            Thread t = new Thread(writeY);
+            t.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("X");
+            }
+            */
+
+            /*2. */
+            new Thread(Go).Start(); // Call Go() on a new Thread
+            Go(); // Call Go() as a function main.
+
+
+            Console.ReadLine();           
+        }
+        static void Go()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("?");
+            }
+        }
+
+        static void writeY()
+        {
+            for (int i = 0; i < 1000; i++) Console.Write("y");
+            Console.WriteLine();
+        }
+    }
+}
